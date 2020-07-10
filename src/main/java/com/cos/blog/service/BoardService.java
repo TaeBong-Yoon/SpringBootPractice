5f3c833b90a5ce01708dab348846bcc987048551
TaeBong-Yoon <fkdlwldxogus@gmail.com>
2020-07-10 16:08:00 +0900

    상세보기,삭제

--- src/main/java/com/cos/blog/service/BoardService.java
+++ src/main/java/com/cos/blog/service/BoardService.java
@@ -1,8 +1,8 @@
 package com.cos.blog.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,13 +17,26 @@ public class BoardService {
 	private BoardRepository boardRepository;
 
 	@Transactional
-	public void write(Board board,User user) {
+	public void write(Board board, User user) {
 		board.setCount(0);
 		board.setUser(user); // 로그인 되어있는 유저 정보
 		boardRepository.save(board);
 	}
-	
-	public List<Board> boardList(){
-		return boardRepository.findAll();
+
+	@Transactional
+	public void delete(int id) {
+		boardRepository.deleteById(id);
+	}
+
+	@Transactional(readOnly = true)
+	public Page<Board> boardList(Pageable pageable) {
+		return boardRepository.findAll(pageable);
+	}
+
+	@Transactional(readOnly = true)
+	public Board boardDetail(int id) {
+		return boardRepository.findById(id).orElseThrow(() -> {
+			return new IllegalArgumentException("글 상세보기 실패");
+		});
 	}
 }
--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -2,13 +2,15 @@ let index = {
 	init:function(){
 		$("#btn-save").on("click",()=>{
 			this.save();
+        });
+        $("#btn-delete").on("click",()=>{
+			this.deleteById();
 		});
 	},
 	save:function(){
 		let data = {
 			title:$("#title").val(),
-			content:$("#content").val(),
-			email:$("#email").val()
+			conetent:$("#content").val()
 		};
         console.log(data)
         $.ajax({
@@ -23,6 +25,19 @@ let index = {
         }).fail(function(error){
             alert(JSON.stringify(error));
         });
-	}
+    },
+    deleteById:function(){
+        var id = $("#id").text();
+        $.ajax({
+            type:"DELETE",
+            url:"/api/board/"+id,
+            dataType:"json"
+        }).done(function(resp){
+            alert('Delete Success.');
+            location.href = "/";
+        }).fail(function(error){
+            alert(JSON.stringify(error));
+        });
+    }
 }
 index.init();
